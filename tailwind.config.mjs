dffbc0adc87cd8683648778de13741dc7812f8e9
Guido Ariel Blanco Azcona <blanco.guido92@gmail.com>
2025-04-18 20:49:39 -0300

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+      extend: {
+        colors: {
+          skin: '#F2D2BD',
+        },
+      },
+    },
+    plugins: [],
+  };
